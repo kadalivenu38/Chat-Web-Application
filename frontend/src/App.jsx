c5d027665e40b8af0f2c799a1a96d3a3cbc467ee
Venu <kadalivenu38@gmail.com>
2026-03-08 13:11:42 +0530

    Frontend created and sidebar component done.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Sidebar from './components/Sidebar'
+import Chatbox from './components/Chatbox'
+import Credits from './pages/Credits'
+import Community from './pages/Community'
+
+function App() {
+
+  return (
+    <>
+      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+        <div className='flex h-screen w-screen'>
+          <Sidebar />
+          <Routes>
+            <Route path='/' element={<Chatbox />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/credits' element={<Credits />} />
+            <Route path='/community' element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
