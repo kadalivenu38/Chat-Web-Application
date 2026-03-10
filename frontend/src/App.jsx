a66db1c08439d89641d87112cd2ff133b49246a4
Venu <kadalivenu38@gmail.com>
2026-03-10 19:55:40 +0530

    Credits and Login page Added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,8 +8,10 @@ import Community from './pages/Community'
 import { assets } from './assets/assets'
 import './assets/prism.css'
 import Loading from './pages/Loading'
+import { useAppContext } from './context/AppContext'
 
 function App() {
+  const {user} = useAppContext()
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const {pathname} = useLocation()
 
@@ -21,17 +23,22 @@ function App() {
     <>
       {!isMenuOpen && <img src={assets.menu_icon} className='absolute top-3 left-3 w-8 h-8 cursor-pointer
         md:hidden not-dark:invert' onClick={()=> setIsMenuOpen(true)} />}
-      <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
-        <div className='flex h-screen w-screen'>
-          <Sidebar isMenuOpen = {isMenuOpen} setIsMenuOpen = {setIsMenuOpen} />
-          <Routes>
-            <Route path='/' element={<Chatbox />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/credits' element={<Credits />} />
-            <Route path='/community' element={<Community />} />
-          </Routes>
+      {user ? (
+        <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
+          <div className='flex h-screen w-screen'>
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path='/' element={<Chatbox />} />
+              <Route path='/credits' element={<Credits />} />
+              <Route path='/community' element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='bg-gradient-to-b from-[#7B1FA2] to-[#C0C0C0] flex items-center justify-center h-screen w-screen'>
+          <Login />
+        </div>
+      )}
     </>
   )
 }
