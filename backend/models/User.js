7335a65d64d95c85e6a93a686e8ec4285409e1af
Venu <kadalivenu38@gmail.com>
2026-03-11 15:33:32 +0530

    Backend setup is done

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  credits: { type: Number, default: 20 },
+});
+
+// Hashing Password Logic
+userSchema.pre("save", async (next) => {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
