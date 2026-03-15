a0a0e774b81a28eb45670378d51575e360158b11
Venu <kadalivenu38@gmail.com>
2026-03-15 23:49:14 +0530

    creditController logic done and payment feature added.

---
+++ backend/controllers/webhooks.js
@@ -0,0 +1,64 @@
+import Stripe from "stripe";
+import Transaction from "../models/Transaction.js";
+import User from "../models/User.js";
+
+const stripeWebhooks = async (req, res) => {
+  const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET);
+  const signature = req.headers["stripe-signature"];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (err) {
+    console.error(err.message);
+    return res.status(400).json({ message: "Webhook Error." });
+  }
+
+  try {
+    switch (event.type) {
+      case "payment_intent.succeeded": {
+        const paymentIntent = event.data.object;
+        const sessionList = await stripe.checkout.sessions.list({
+          payment_intent: paymentIntent.id,
+        });
+        const session = sessionList.data[0];
+        const { transactionId, appId } = session.metadata;
+
+        if (appId === "quickgpt") {
+          const transaction = await Transaction.findOne({
+            _id: transactionId,
+            isPaid: false,
+          });
+
+          await User.updateOne(
+            { _id: transaction.userId },
+            {
+              $inc: {
+                credits: transaction.credits,
+              },
+            },
+          );
+
+          transaction.isPaid = true;
+          await transaction.save();
+        } else {
+          return res.status(400).json({ message: "Ignored event: Invalid app" });
+        }
+        break;
+      }
+
+      default:
+        console.log("Unhandled event type:", event.type);
+        break;
+    }
+    return res.json({ received: true });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({ message: "Internal Server Error." });
+  }
+};
+
+export { stripeWebhooks };
--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ import dbConn from "./config/db.js";
 import userRouter from "./routes/userRoutes.js";
 import chatRouter from "./routes/chatRoutes.js";
 import msgRouter from "./routes/messageRoutes.js";
+import creditRouter from "./routes/creditRoutes.js";
+import { stripeWebhooks } from "./controllers/webhooks.js";
 
 const app = express();
 await dbConn();
+
+// stripe webhooks
+app.post(
+  "/api/stripe",
+  express.raw({ type: "application/json" }),
+  stripeWebhooks,
+);
 
 // middlewares
 app.use(cors({ origin: "*" }));
@@ -19,6 +28,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRouter);
 app.use("/chat", chatRouter);
 app.use("/message", msgRouter);
+app.use("/credits", creditRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
